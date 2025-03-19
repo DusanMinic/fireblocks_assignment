441e0e5a47a9c8a67a95e7b7383fb4c2014b5f79
Dušan Minić <dusanminic@Dusans-MacBook-Pro.local>
2025-03-19 19:16:09 +0100

    feat: define reactive cell types
    - Added interfaces for the InputCell and ComputedCell
    - Created a union type ReactiveCell

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,21 @@ if (!filePath) {
     console.error('Please provide an input file as argument.');
     process.exit(1);
 }
+
+
+// holds a literal numeric value.
+interface InputCell {
+    type: 'input';
+    value: number;
+}
+
+// holds a formula (without the '='), a list of dependencies, and its computed numeric value
+interface ComputedCell {
+    type: 'computed';
+    formula: string;
+    dependencies: number[];
+    value: number;
+}
+
+// A union type for any reactive cell.
+type ReactiveCell = InputCell | ComputedCell;
